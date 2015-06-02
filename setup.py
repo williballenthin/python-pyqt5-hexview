1d97b2c887eac4546d17296cd63cac1c0e0adbd0
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-06-02 13:53:30 -0400

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from hexview import __version__
+from setuptools import setup
+
+
+description = "PyQt5 hex viewer widget."
+setup(name="python-pyqt5-hexview",
+      version=__version__,
+      description=description,
+      long_description=description,
+      author="Willi Ballenthin",
+      author_email="willi.ballenthin@gmail.com",
+      url="https://github.com/williballenthin/python-pyqt5-hexview",
+      license="Apache 2.0 License",
+      packages=["hexview"])
